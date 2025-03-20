4d59be4146ce1c5f65ccf8e512a8b2da082e7ee4
matthughesr <matthughesr@gmail.com>
2025-03-19 20:43:11 -0600

    Added Folders. Changed startup window

--- Search/wndSearch.xaml.cs
+++ Search/wndSearch.xaml.cs
@@ -12,14 +12,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace GroupProject
+namespace GroupProject.Search
 {
     /// <summary>
-    /// Interaction logic for SearchWindow.xaml
+    /// Interaction logic for wndSearch.xaml
     /// </summary>
-    public partial class SearchWindow : Window
+    public partial class wndSearch : Window
     {
-        public SearchWindow()
+        public wndSearch()
         {
             InitializeComponent();
         }
